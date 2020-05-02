61128cebddddd6b4f27ac56c3d6edad94da69e4f
MarvRoldan <marvinroldan_@hotmail.com>
2020-05-01 18:46:48 -0600

    Populating the page with info

--- index.php
+++ index.php
@@ -1,13 +1,21 @@
 <?php
 
+$randomMobJSONString = file_get_contents('https://api.jikan.moe/v3/anime/32182/');
+$randomMobObject = json_decode($randomMobJSONString);
+$randomMob = $randomMobObject;
+
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>API-Assignment</title>
+    <title>Mob Psycho 100 - API</title>
 </head>
 <body>
-    
+    <h1>Information</h1>
+    <dl>
+        <dt>Title:</dt>
+        <dd><?php echo $randomMob->title; ?></dd>
+    </dl>
 </body>
 </html>
